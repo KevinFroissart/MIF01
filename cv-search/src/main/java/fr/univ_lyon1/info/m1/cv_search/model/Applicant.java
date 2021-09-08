dacbb09ca3641998e34eeefb1a0f1c0c1bb55f3f
FROISSART KEVIN p2002504 <p2002504@b710l0116.univ-lyon1.fr>
2021-09-08 11:11:47 +0200

    Javadoc for models

--- cv-search/src/main/java/fr/univ_lyon1/info/m1/cv_search/model/Applicant.java
+++ cv-search/src/main/java/fr/univ_lyon1/info/m1/cv_search/model/Applicant.java
@@ -7,18 +7,36 @@ public class Applicant {
     Map<String, Integer> skills = new HashMap<>();
     String name;
 
+    /** 
+     * Returns applicant's skill level.
+     * @param string the skill name.
+     * @return int the skill level.
+     */
     public int getSkill(String string) {
         return skills.getOrDefault(string, 0);
     }
 
+    /** 
+     * Sets applicant's skill.
+     * @param string the skill.
+     * @param value the skill level.
+     */
     public void setSkill(String string, int value) {
         skills.put(string, value);
     }
-
+    
+    /** 
+     * Returns applicant's name.
+     * @return String the applicant's name.
+     */
     public String getName() {
         return name;
     }
-
+    
+    /** 
+     * Sets applicant's name.
+     * @param name the applicant's name.
+     */
     public void setName(String name) {
         this.name = name;
     }
