c570527c3049848a0dbddde3d7774543aa9f273c
FROISSART KEVIN p2002504 <p2002504@b710l0116.univ-lyon1.fr>
2021-09-10 12:57:37 +0200

    SkillList Model + Observer/Observable DP

--- cv-search/src/main/java/fr/univ_lyon1/info/m1/cv_search/App.java
+++ cv-search/src/main/java/fr/univ_lyon1/info/m1/cv_search/App.java
@@ -1,5 +1,7 @@
 package fr.univ_lyon1.info.m1.cv_search;
 
+import fr.univ_lyon1.info.m1.cv_search.controller.CvController;
+import fr.univ_lyon1.info.m1.cv_search.model.SkillList;
 import fr.univ_lyon1.info.m1.cv_search.view.JfxView;
 import javafx.application.Application;
 import javafx.stage.Stage;
@@ -14,7 +16,10 @@ public class App extends Application {
      */
     @Override
     public void start(Stage stage) throws Exception {
-        new JfxView(stage, 600, 600);
+        SkillList skillList = new SkillList();
+        JfxView jfxView = new JfxView(stage, 600, 600, new CvController());
+        skillList.addObserver(jfxView);
+        skillList.customNotify();
     }
 
 
