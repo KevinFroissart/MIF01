e0d947843d7dd8ace77dc7d40bdc96a258f1e6ac
p1805596 <hamza.touhardji@etu-lyon1.fr>
2021-10-24 19:04:35 +0200

    add rapport

--- cv-search/src/test/java/fr/univ_lyon1/info/m1/cv_search/SkillListTest.java
+++ cv-search/src/test/java/fr/univ_lyon1/info/m1/cv_search/SkillListTest.java
@@ -1,6 +1,5 @@
 package fr.univ_lyon1.info.m1.cv_search;
 
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import fr.univ_lyon1.info.m1.cv_search.model.SkillList;
@@ -9,29 +8,19 @@ import static org.hamcrest.MatcherAssert.assertThat;
 
 public class SkillListTest {
 
-    private SkillList s;
-
-    @BeforeEach
-    public void setUp() {
-        // Given 
-        s = new SkillList();;
-    }
-
     @Test
     public void testAddSkill() {
         System.out.println("--------------SkillListTest---------------");
-        // When
+        SkillList s = new SkillList();
         s.addSkill("c");
         s.addSkill("c++");
 
         Boolean found = s.toString().contains("c++");
         found = s.toString().contains("c");
-        //Then
         assertThat(found, is(true));
 
         s.removeSkill("c++");
         found = s.toString().contains("c++");
-        //Then
         assertThat(found, is(false));
     }
 }
